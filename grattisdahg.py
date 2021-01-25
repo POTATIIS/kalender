ee91700d2020d8fe75eb921db16c6e15d5919713
POTATIIS <45688745+POTATIIS@users.noreply.github.com>
2021-01-25 14:22:38 +0100

    övningsuppgift 5.2

--- grattisdahg.py
+++ grattisdahg.py
@@ -1,3 +1,9 @@
-from datetime import date 
-today = date.today()
-print(today)
+import datetime   #lägger till funktionen att jobba med tid
+while True:       #låter dig köra programet utan att behöva starta om
+    dt = datetime.datetime.now() #sätter variabeln för tiden nu
+    personnummer = input("Skriv ditt personnummer(ååååmmdd): ") #säger vad man ska skriva när man kör programmet
+    dtpers = dt.strptime(personnummer, '%Y%m%d')
+    if (dt.month == dtpers.month and dt.day == dtpers.day):
+        print("Grattis på födelsedagen!")
+    else:
+        print("Inte födelsedag!")
